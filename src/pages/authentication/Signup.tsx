abad802c3bb4afc47bccd5877d8a445b424a5fe8
Juan Gonzalez <jgonzalez.wj@gmail.com>
2025-07-31 17:12:18 -0600

    Final styling of signup page buttons

--- src/pages/authentication/Signup.tsx
+++ src/pages/authentication/Signup.tsx
@@ -1,22 +1,30 @@
 import { IonPage, IonHeader, IonToolbar, IonRouterLink, IonTitle, IonText, IonContent, IonImg, IonButton } from '@ionic/react';
 import './Signup.css';
 import appLogo from '../../assets/images/app-logo.jpg';
+import { useState } from 'react';
 
 const Signup: React.FC = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+
+  }
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className='bg-[#F0E4CE] flex flex-col items-center justify-evenly h-full w-full p-4'>
           <IonImg src={appLogo} className='w-full mx-auto' />
           <div className='flex flex-col items-center justify-center w-full'>
-            <IonButton className='w-full bg-[#F5BC55]'>Continue With Email</IonButton>
-            <div className='flex flex-col items-center justify-center w-full'>
-              <div></div>
+            <IonButton className={isClicked ? 'w-full signButton-clicked' : 'w-full signButton'} onClick={handleClick}>Continue with Email</IonButton>
+            <div className='myText flex items-center justify-center w-full'>
+              <div className='flex flex-col w-full items-center justify-center line'></div>
               <IonText>Or use social for sign up</IonText>
-              <div></div>
+              <div className='flex flex-col w-full items-center justify-center line'></div>
             </div>
-            <IonButton className='mt-4 w-full bg-[#2B4D3F]'>Continue With Google</IonButton>
-            <IonText>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
+            <IonButton className={isClicked ? 'googleButton-clicked w-full' : 'googleButton w-full'} onClick={handleClick}>Continue with Google</IonButton>
+            <IonText class='myText'>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
           </div>
         </div>
       </IonContent>
