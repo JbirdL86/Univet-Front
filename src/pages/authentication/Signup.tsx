c60a54b34b1b3d175b7fa5b9b19a8e3f97d40ecc
Juan Gonzalez <jgonzalez.wj@gmail.com>
2025-07-30 18:18:27 -0600

    Add buttons styling and extra text

--- src/pages/authentication/Signup.tsx
+++ src/pages/authentication/Signup.tsx
@@ -1,4 +1,4 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonButton } from '@ionic/react';
+import { IonPage, IonHeader, IonToolbar, IonRouterLink, IonTitle, IonText, IonContent, IonImg, IonButton } from '@ionic/react';
 import './Signup.css';
 import appLogo from '../../assets/images/app-logo.jpg';
 
@@ -9,8 +9,14 @@ const Signup: React.FC = () => {
         <div className='bg-[#F0E4CE] flex flex-col items-center justify-evenly h-full w-full p-4'>
           <IonImg src={appLogo} className='w-full mx-auto' />
           <div className='flex flex-col items-center justify-center w-full'>
-            <IonButton className='w-full'>Continue With Email</IonButton>
-            <IonButton className='mt-4 w-full'>Continue With Google</IonButton>
+            <IonButton className='w-full bg-[#F5BC55]'>Continue With Email</IonButton>
+            <div className='flex flex-col items-center justify-center w-full'>
+              <div></div>
+              <IonText>Or use social for sign up</IonText>
+              <div></div>
+            </div>
+            <IonButton className='mt-4 w-full bg-[#2B4D3F]'>Continue With Google</IonButton>
+            <IonText>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
           </div>
         </div>
       </IonContent>
