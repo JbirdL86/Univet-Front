fb984c36f518a67119bd77c52416b843346128ab
Nelsino Francisco <nelsino.francisco@gmail.com>
2025-08-04 21:12:18 -0300

    push initial draft of sign up steps

--- src/pages/authentication/Signup.tsx
+++ src/pages/authentication/Signup.tsx
@@ -8,19 +8,15 @@ import appLogo from '../../assets/images/app-logo.jpg';
 import PopoverForm from '../../components/PopoverForm';
 
 const Signup: React.FC = () => {
-  const [showPopover, setShowPopover] = useState(false);
-  //const [popoverEvent, setPopoverEvent] = useState<any>();
+  const [formStep, setFormStep] = useState(1);
   const [isClicked, setIsClicked] = useState(false);
 
-  const handlePresentPopover = (e: React.MouseEvent) => {
-    e.preventDefault();
-   // setPopoverEvent(e.nativeEvent);
-    setShowPopover(true);
-    handleClick();
+  const nextStep = () => {
+    setFormStep(formStep + 1);
   };
 
-  const handleDismissPopover = () => {
-    setShowPopover(false);
+  const prevStep = () => {
+    setFormStep(formStep - 1);
   };
 
   const handleFormSubmit = (data: any) => {
@@ -29,7 +25,6 @@ const Signup: React.FC = () => {
 
   const handleClick = () => {
     setIsClicked(!isClicked);
-
   }
 
   const googleLogin = useGoogleLogin({
@@ -47,38 +42,36 @@ const Signup: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonPopover
-          isOpen={showPopover}
-          onDidDismiss= {handleDismissPopover}
-          //event = {popoverEvent}
-          className="fullscreen-popover"
-        >
-          <PopoverForm onDismiss={handleDismissPopover}></PopoverForm>
-        </IonPopover>
-        <div className='bg-[#F0E4CE] flex flex-col items-center justify-evenly h-full w-full p-4'>
-          <IonImg src={appLogo} className='w-full mx-auto' />
-          <div className='flex flex-col items-center justify-center w-full'>
-            <IonButton className={isClicked ? 'w-full signButton-clicked' : 'w-full signButton'} onClick={(e) => {
-              e.persist;
-              //setPopoverEvent(e);
-              setShowPopover(true);
-            }}>
-              Continue with Email
-            </IonButton>
-            <div className='myText flex items-center justify-center w-full'>
-              <div className='flex flex-col w-full items-center justify-center line'></div>
-              <IonText>Or use social for sign up</IonText>
-              <div className='flex flex-col w-full items-center justify-center line'></div>
+        { 
+           formStep === 1 ? (
+            <div className='bg-[#F0E4CE] flex flex-col items-center justify-evenly h-full w-full p-4'>
+              <IonImg src={appLogo} className='w-full mx-auto' />
+              <div className='flex flex-col items-center justify-center w-full'>
+                <IonButton className={isClicked ? 'w-full signButton-clicked' : 'w-full signButton'} onClick={(e) => {
+                  nextStep();
+                }}>
+                  Continue with Email
+                </IonButton>
+                <div className='myText flex items-center justify-center w-full'>
+                  <div className='flex flex-col w-full items-center justify-center line'></div>
+                  <IonText>Or use social for sign up</IonText>
+                  <div className='flex flex-col w-full items-center justify-center line'></div>
+                </div>
+                <IonButton
+                  className={isClicked ? 'googleButton-clicked w-full' : 'googleButton w-full'}
+                  onClick={() => { googleLogin();}}
+                >
+                Continue with Google
+                </IonButton>
+                <IonText class='myText'>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
+              </div>
             </div>
-            <IonButton
-              className={isClicked ? 'googleButton-clicked w-full' : 'googleButton w-full'}
-              onClick={() => { googleLogin(); handleClick();}}
-            >
-            Continue with Google
-            </IonButton>
-            <IonText class='myText'>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
-          </div>
-        </div>
+          ) : formStep === 2 ? (
+              <PopoverForm onDismiss={prevStep}></PopoverForm>
+          ) : (
+            <div></div>
+          )
+        }
       </IonContent>
     </IonPage>
   )
