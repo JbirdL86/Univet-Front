1d5b6f7e91f48892504355241be339c63ed3ba7d
Juan Gonzalez <jgonzalez.wj@gmail.com>
2025-08-04 18:44:38 -0600

    Change initial signup to steps

---
+++ src/components/authentication/signup/Step1.tsx
@@ -0,0 +1,69 @@
+import { IonPage, IonPopover, IonRouterLink, IonText, IonContent, IonImg, IonButton } from '@ionic/react';
+import { useState } from 'react';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import './Signup.css';
+import '../../components/PopoverForm.css'
+import '../../components/PopoverForm'
+import appLogo from '../../assets/images/app-logo.jpg';
+
+const Step1: React.FC = () => {
+  const [formStep, setFormStep] = useState(1);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const nextStep = () => {
+    setFormStep(formStep + 1);
+  };
+
+  const prevStep = () => {
+    setFormStep(formStep - 1);
+  };
+
+  const handleFormSubmit = (data: any) => {
+    console.log('Form data submitted:', data);
+  };
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  }
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: tokenResponse => {
+        console.log('Google token received:', tokenResponse);
+        // This is where you would send the tokenResponse.access_token or tokenResponse.credential
+        // to your Rails backend for verification and user sign-in.
+        // Example: sendTokenToServer(tokenResponse);
+    },
+    onError: () => {
+        console.log('Login Failed');
+    },
+  });
+
+  return (
+    <IonContent fullscreen>
+         <div className='bg-[#F0E4CE] flex flex-col items-center justify-evenly h-full w-full p-4'>
+            <IonImg src={appLogo} className='w-full mx-auto' />
+            <div className='flex flex-col items-center justify-center w-full'>
+              <IonButton className={isClicked ? 'w-full signButton-clicked' : 'w-full signButton'} onClick={(e) => {
+                nextStep();
+              }}>
+                Continue with Email
+              </IonButton>
+              <div className='myText flex items-center justify-center w-full'>
+                <div className='flex flex-col w-full items-center justify-center line'></div>
+                <IonText>Or use social for sign up</IonText>
+                <div className='flex flex-col w-full items-center justify-center line'></div>
+              </div>
+              <IonButton
+                className={isClicked ? 'googleButton-clicked w-full' : 'googleButton w-full'}
+                onClick={() => { googleLogin();}}
+              >
+              Continue with Google
+              </IonButton>
+              <IonText class='myText'>Already have an account? <IonRouterLink>Log In</IonRouterLink></IonText>
+            </div>
+          </div>
+    </IonContent>
+  )
+}
+
+export default Step1;
